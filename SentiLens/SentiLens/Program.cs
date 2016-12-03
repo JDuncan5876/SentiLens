4d0f3a3142f39501f4a2ddc4ad23cc1a6cb25d66
JDuncan5876 <jduncan5876@gmail.com>
2016-12-02 22:27:00 -0500

    Initial commit

--- SentiLens/SentiLens/Program.cs
+++ SentiLens/SentiLens/Program.cs
@@ -10,7 +10,7 @@ namespace SentiLens
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");//
         }
     }
 }
